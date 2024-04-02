689f65cde8e6aed2d972a174895c97c3fc842196
Stéphane Rident <109585662+Golushi@users.noreply.github.com>
2024-04-02 08:52:18 +0200

    suite

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-
+import Heading from "./components/Heading"
+import { Section } from "./components/Section"
 
 function App() {
   
 
-  return <h1>Hello!</h1>
+  return (
+    <>
+    <Heading title={"Hello!"} />
+    <Section>This is my Section.</Section>
+    </>
+  )
 }
 
 export default App
